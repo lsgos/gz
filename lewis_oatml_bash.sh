3a2f0c4348b758c56ccc3b9ede66f307ea14be49
mizunt1 <mizunt@gmail.com>
2021-05-26 09:26:54 +0100

    bash script removing all previously ran runs

--- lewis_oatml_bash.sh
+++ lewis_oatml_bash.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:1
-#SBATCH --job-name="dataaug"
-#SBATCH --array=39%16
+#SBATCH --job-name="gz"
+#SBATCH --array=0-320%8
+#SBATCH	--exclude="oat1"
 export CONDA_ENVS_PATH=/scratch-ssd/$USER/conda_envs
 export CONDA_PKGS_DIRS=/scratch-ssd/$USER/conda_pkgs
 export TMPDIR=/scratch/${USER}/tmp
@@ -15,23 +16,59 @@ source /scratch-ssd/oatml/miniconda3/bin/activate gz_by_hand
 
 #pip -e batchbald_redux
 ACQUISITIONS=(random BALD)
-ENCODER=(True)
-BAR_NO_BAR=(False)
-N_SEEDS=1
+ENCODER=(True False)
+DATA_AUG=(True False)
+SPATIAL_TRANSFORMER=(True False)
+SPATIAL_VAE=(True False)
+BAR_NO_BAR=(False True)
+N_SEEDS=5
 N_ACQUISITIONS=${#ACQUISITIONS[@]}
 N_ENCODER=${#ENCODER[@]}
 N_BAR=${#BAR_NO_BAR[@]}
+N_DATA_AUG=${#DATA_AUG[@]}
+N_SPATIAL_TRANSFORMER=${#SPATIAL_TRANSFORMER[@]}
+N_SPATIAL_VAE=${#SPATIAL_VAE[@]}
 SEED=$(( SLURM_ARRAY_TASK_ID % N_SEEDS ))
 ACQUISITIONS_IDX=$(( (SLURM_ARRAY_TASK_ID / N_SEEDS) % N_ACQUISITIONS ))
 ENCODER_IDX=$(( (SLURM_ARRAY_TASK_ID / (N_ACQUISITIONS * N_SEEDS)) % N_ENCODER ))
 BAR_IDX=$(( (SLURM_ARRAY_TASK_ID / (N_ACQUISITIONS * N_SEEDS * N_ENCODER)) % N_BAR  ))
-srun python -u al_test.py with 'dataset=gz' \
-                               bar_no_bar=${BAR_NO_BAR[$BAR_IDX]} \
-                               use_pose_encoder=${ENCODER[$ENCODER_IDX]} \
-                               seed=$SEED \
-                               acquisition=${ACQUISITIONS[$ACQUISITIONS_IDX]} \
-                               'pretrain_epochs=100' \
-                               'transform_spec=["Translation", "Rotation"]' \
-                               classify_from_z=False \
-                               pixel_likelihood=laplace \
-                               -F output_rmse_fixed
+DATA_AUG_IDX=$(( (SLURM_ARRAY_TASK_ID / (N_ACQUISITIONS * N_SEEDS * N_ENCODER * N_BAR)) % N_DATA_AUG  ))
+SPATIAL_TRANSFORMER_IDX=$(( (SLURM_ARRAY_TASK_ID / (N_ACQUISITIONS * N_SEEDS * N_DATA_AUG * N_BAR * N_ENCODER)) % N_SPATIAL_TRANSFORMER  ))
+SPATIAL_VAE_IDX=$(( (SLURM_ARRAY_TASK_ID / (N_ACQUISITIONS * N_SEEDS * N_DATA_AUG * N_SPATIAL_TRANSFORMER *N_BAR * N_ENCODER )) % N_SPATIAL_VAE  ))
+
+nvidia-smi -q | grep UUID
+
+COMPLETED_TASKS=(122 114 31 105 117 110 87 126 70 113 106 123 104 47 58 111 99 102 10 116 119 109 98 101 120 108 100 107 15 45 11 19 127 118 18 24 25 121 56 115 14 22 26 112 103)
+
+declare -A array
+for n in ${COMPLETED_TASKS[@]}
+do
+    array[$n]=1
+done
+
+
+# ENCODER must be TRUE for Spatial vae and spatial transformer
+if [[ ${SPATIAL_TRANSFORMER[$SPATIAL_TRANSFORMER_IDX]} == True ]] && [[ ${ENCODER[$ENCODER_IDX]} == False ]]; then
+    echo SKIPPING
+elif [[ ${SPATIAL_VAE[$SPATIAL_VAE_IDX]} == True ]] && [[ ${ENCODER[$ENCODER_IDX]} == False ]]; then
+    echo SKIPPING
+elif [[ ${SPATIAL_VAE[$SPATIAL_VAE_IDX]} == True ]] && [[ ${SPATIAL_TRANSFORMER[$SPATIAL_TRANSFORMER_IDX]} == True ]]; then
+    echo SKIPPING
+elif [[  ${array[$SLURM_ARRAY_TASK_ID]}  ]]; then
+     echo SKIPPING
+else
+    srun python -u al_test.py with 'dataset=gz' \
+	 bar_no_bar=${BAR_NO_BAR[$BAR_IDX]} \
+	 use_pose_encoder=${ENCODER[$ENCODER_IDX]} \
+	 seed=$SEED \
+	 data_aug=${DATA_AUG[$DATA_AUG_IDX]} \
+	 spatial_vae=${SPATIAL_VAE[$SPATIAL_VAE_IDX]} \
+	 spatial_transformer=${SPATIAL_TRANSFORMER[$SPATIAL_TRANSFORMER_IDX]} \
+	 acquisition=${ACQUISITIONS[$ACQUISITIONS_IDX]} \
+	 'pretrain_epochs=100' \
+	 'transform_spec=["Translation", "Rotation"]' \
+	 classify_from_z=False \
+	 pixel_likelihood=laplace \
+	 -F output_rmse_fixed
+fi
+ 
